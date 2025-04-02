bb481f52219bb3c34ce84d995f95102b6bcf60d3
jason13official <jason13official@gmail.com>
2025-04-01 22:45:28 -0400

    public boolean for soul fire'd before I forget

--- common/src/main/java/com/cursee/danger_close/DangerClose.java
+++ common/src/main/java/com/cursee/danger_close/DangerClose.java
@@ -1,10 +1,15 @@
 package com.cursee.danger_close;
 
+import com.cursee.danger_close.platform.Services;
 import net.minecraft.resources.ResourceLocation;
 
 public class DangerClose {
 
-    public static void init() {}
+    public static boolean SOUL_FIRE_D_INSTALLED = false;
+
+    public static void init() {
+        SOUL_FIRE_D_INSTALLED = Services.PLATFORM.isModLoaded("soul_fire_d");
+    }
 
     public static ResourceLocation identifier(String value) {
         return new ResourceLocation(Constants.MOD_ID, value);
