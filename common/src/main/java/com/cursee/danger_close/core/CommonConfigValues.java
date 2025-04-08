65766be09e59fcf88f9ca8e45562e25c00fc9842
jason13official <jason13official@gmail.com>
2025-04-07 23:26:26 -0400

    fix config options

--- common/src/main/java/com/cursee/danger_close/core/CommonConfigValues.java
+++ common/src/main/java/com/cursee/danger_close/core/CommonConfigValues.java
@@ -2,17 +2,17 @@ package com.cursee.danger_close.core;
 
 public class CommonConfigValues {
 
-    public static boolean shouldDetect = false;
+    public static boolean shouldDetect = true;
 
     public static boolean shouldTorchImmolate = false;
     public static boolean shouldSoulTorchImmolate = false;
 
-    public static boolean shouldCampfireImmolate = false;
-    public static boolean shouldSoulCampfireImmolate = false;
+    public static boolean shouldCampfireImmolate = true;
+    public static boolean shouldSoulCampfireImmolate = true;
 
-    public static boolean shouldStonecutterCut = false;
+    public static boolean shouldStonecutterCut = true;
 
-    public static boolean shouldBlazeImmolate = false;
-    public static boolean shouldMagmaBlockImmolate = false;
+    public static boolean shouldBlazeImmolate = true;
+    public static boolean shouldMagmaBlockImmolate = true;
     public static boolean shouldMagmaCubeImmolate = false;
 }
